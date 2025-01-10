f9bf0e32411ced1abe36430596cb76a00cd2a0a7
jrzimmm <jrzimmerman@cps.edu>
2025-01-10 12:07:19 -0600

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,294 @@
+def drawCharizard():
+  print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡜⢛⣤⠀⠀⠀⣀⠜⠛⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠁⣾⠁⢀⣠⠴⠉⢀⣴⡋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠛⢱⡷⠛⠛⠛⠀⠀⣄⣺⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡰⠋⠉⠀⠀⠀⠀⠀⠀⠀⠸⢇⡀⠀⠀⠀⠀⠀⠀⣰⠶⣂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⣴⠇⣠⠄⠀⢠⣀⢤⣤⠀⠀⠀⢇⠈⢿⣁⠀⠀⠀⠀⠀⠳⢌⡩⢻⣤⣄⣿⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠎⠉⣶⠀⠀⠀⠀⠿⣶⡏⠷⣴⣿⡏⣀⠏⠀⠀⠀⠀⠀⠀⠿⣀⠀⠀⠀⠀⠀⠈⣳⠀⢀⠛⡉⠉⠱⠶⠶⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠙⢀⡸⠀⠀⠀⢀⣀⠬⠛⠑⠃⠀⠀⠀⠀⣠⣤⣤⠄⠀⠀⠀⠘⣿⡀⠀⠀⠀⣀⡀⠀⣿⣁⣦⣿⣿⣿⣿⣤⣤⠀⣻⠄⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣀⡰⠎⠁⠀⣸⡇⠀⠀⢰⡎⠉⣀⠀⠀⠀⠀⣀⡴⡶⠋⠀⠀⠀⠀⠀⠀⠀⢔⡇⠀⠀⠀⣿⣇⠀⠈⢷⡍⣿⣿⣿⣿⣿⣿⣾⢘⣶⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⢠⠜⠁⢠⣤⣾⣿⡨⣹⠀⠀⠀⢸⡄⠃⠀⣠⡞⣿⢉⣁⣠⣶⣾⣿⡟⠀⠀⠀⠈⠈⢹⡆⠀⢀⡏⢸⡄⠀⢸⣇⣛⣿⣿⣿⣿⣿⣿⡇⠛⡤⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⡈⠇⣶⣿⣿⣿⣿⣿⡧⠹⣀⠀⠀⠀⠈⠶⠾⢧⣭⣴⠎⠉⠉⠀⠈⢹⡅⠀⠀⠀⠀⠀⡸⢇⣀⠟⠀⢸⠄⠀⠀⢸⡇⣿⣿⣿⣿⣿⣿⣷⣎⠱⡆⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⡇⢨⣿⣿⣿⣿⣿⣿⣿⡇⣿⡀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠄⢸⠂⢀⢸⡇⠀⡀⢸⡏⣴⣿⣿⣿⣿⣿⣿⣿⣿⣤⠸⡇⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠇⢸⣿⣿⣿⣿⣿⣿⣿⣷⣆⣿⣟⠷⠶⢦⣀⣀⡀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢰⠀⠈⣶⠶⢁⣰⠶⢋⣹⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠸⣅⠀⠀⠀
+  ⠀⠀⠀⡾⢠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣥⡙⠓⣤⠀⣀⡤⠤⢚⣻⠇⠀⠀⠀⠸⠀⠀⠀⠛⣤⣚⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠓⡄⠀
+  ⠀⠀⣶⢁⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⡿⠿⠶⣄⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⠛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⢱⡆
+  ⠀⢰⠉⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠙⠛⠛⠛⠛⠁⠀⡀⠋⢩⡖⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⢂⠀⠈⠉⠉⢻⠛⢻⣿⠟⠛⣿⣿⣿⣿⣿⣿⣿⣿⢸⡇
+  ⢸⡇⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⣸⣦⣠⣄⡄⠀⠀⠀⡌⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⣴⣶⣶⣦⢀⠈⢱⠀⠀⠀⠈⠉⣿⣿⣿⣿⣒⡎⠁
+  ⠘⢣⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠰⣿⣿⣿⣿⡟⠛⠃⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⠀⠘⢿⣿⣿⡀⠀⣿⠀⠀⠀⠀⣿⣿⣿⡿⣘⡇⠀
+  ⠀⠸⣄⢻⣿⣿⣿⣿⣿⣿⣿⠋⠀⠘⣿⣿⠷⠀⠀⠈⠿⢿⣿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠸⠿⣛⠇⠀⠀⠿⠳⣆⠀⠐⣿⣿⡇⣿⠀⠀
+  ⠀⠀⢿⡎⢹⣿⣿⣿⣿⣿⠏⠀⠀⣸⠋⠁⠀⠀⠀⠀⠀⠈⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢛⡆⠀⣶⠉⠀⡀⠀⠀⢻⡌⣷⠘⢻⡏⣭⠛⠀⠀
+  ⠀⠀⠀⠿⣞⠻⣿⣿⣿⡇⠀⠀⢸⡇⠐⣴⣦⠀⢀⣾⢷⡆⢨⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢱⣾⣇⠒⡎⠉⣿⠒⢺⡷⠋⠀⢸⣧⡿⠀⠀⠀
+  ⠀⠀⠀⠀⢹⡇⠻⣿⣿⣇⠄⠀⠈⠙⣭⠛⢻⡄⣨⠻⡎⠙⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⣯⡛⠚⠁⠐⣭⡿⠋⠁⠀⠀⠈⠉⠁⠀⠀⠀
+  ⠀⠀⠀⠀⠀⢸⠷⣘⠻⣿⡏⠀⢄⣀⠀⠀⡰⠎⠉⠉⠀⠀⠀⠀⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠼⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠘⠁⠉⢳⣭⣿⡄⠀⠀⣠⡿⠀⠀⠀⠀⠀⠀⠀⠀⢘⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠃⠀⠛⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠼⣀⠀⠀⠒⠤⢄⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⡘⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡌⠀⠀⠀⢣⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠋⣄⠀⠀⠀⠀⢸⠉⠘⠤⠄⠀⠀⠀⠀⠀⠀⢀⣾⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠅⠠⠤⠤⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡼⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⠋⠀⣠⡀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⢢⣼⡇⠀⠀⠀⠀⠀⠀⠀⣲⣽⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡴⠆⠀⠀⠀⠻⠃⢸⡏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠀⠀⠀⣀⠿⠀⣀⣿⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⠶⠾⠉⣶⠀⠀⠀⠀⠀⠀⣘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠟⠉⠀⠀⠀⠉⠀⠀⣿⠍⠉⠑⠛⠛⠛⠛⠛⠛⠛⠋⠉⠉⠁⠀⠀⠀⣿⠀⠀⠀⢀⣀⡀⠉⣹⡷⣦⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠿⢦⣀⠶⠶⣀⢠⠶⣁⢀⡿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠶⢆⣄⡾⠛⠳⣶⠛⠳⢦⠤⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢽⠀⣼⣿⠀⣤⣿⣯⡀⡭⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠒⢢⣽⠒⠒⠚⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠘⠋⠀⠀⠘⠙⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+
+def drawCharmander():
+  print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⠤⠤⠤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣤⠞⠉⠀⠀⠀⠀⠀⠀⠈⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⢠⡞⠁⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢀⣿⠰⣦⠀⠀⠀⠀⠀⠀⢀⣼⢡⠾⣷⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢸⣿⢷⠛⠀⠀⠀⠀⠀⠀⠀⠉⢸⡀⢸⣇⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢸⣿⣾⡆⠀⠀⠀⠀⠀⠀⠀⠀⢸⠿⣿⢹⡀⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢸⣏⢉⡇⠀⠀⠀⠀⠀⠀⠀⠀⠸⣦⡤⠞⠃⢸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⢠⡟⡈⠉⠀⠀⡀⠀⠀⠀⡀⠀⠀⠀⠀⢀⣠⠄⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⣤⣀⠀⠀⠀
+  ⠀⠀⠀⠸⣇⠙⢶⢤⣄⣁⣀⣀⣈⣁⣤⠤⠔⢾⣽⠁⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣄⡿⢿⣿⡆⠀⠀
+  ⠀⠀⠀⠀⠙⠦⣜⠛⠳⣄⡀⢀⡼⠟⠛⠛⣶⠞⠁⠀⡾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⡇⠈⠁⡇⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠈⠙⠒⠤⣭⡟⢒⣒⠒⣋⠁⠀⠀⢸⣅⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡞⠙⠀⠀⠀⣧⠀⠀
+  ⠀⠀⠀⠀⠀⠀⢀⣠⠴⠚⢩⡇⣠⠿⠛⠧⣄⠀⠀⠀⠀⠀⠉⠙⠲⢤⣀⡀⠀⣀⡀⠀⠀⠀⢠⡾⠁⠀⠀⠀⠀⢸⠀⠀
+  ⢠⣴⣶⣾⠶⠚⠁⠀⠀⠀⢸⡼⠁⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠉⠋⢻⣷⡶⠀⠀⡟⠀⠀⠀⠀⠀⣠⣿⣇⡀
+  ⠈⠻⢿⡀⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⢠⣿⣆⣨⣿⡆⠀⠀⢻⡄⠀⠀⠀⠀⠈⠛⣹⠁
+  ⠀⠀⢾⠽⠒⠒⠒⠒⠒⠒⡇⠀⠀⠀⠀⠀⠀⠀⠀⠸⡍⠓⠒⢶⡒⠒⠒⠚⠉⠁⠀⠀⠀⠀⠀⠹⡄⠀⠀⠀⣠⠞⠉⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡀⠀⠈⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣳⠶⡞⠃⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠹⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⢠⡇⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⣠⠾⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡷⠀⠀⠀⠘⢦⠀⠀⠀⠀⠀⠀⣠⡾⠁⠀⣼⠁⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⣴⠋⠁⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠏⠀⠀⠀⠀⠀⠘⣧⣤⣤⣤⠴⠚⠉⠀⢀⣾⡏⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣼⠁⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⢀⡴⢫⡞⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠙⠶⣄⡀⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⡇⢀⣀⡤⠖⢋⡴⠋⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠘⣆⡀⠀⠀⠀⠀⢀⣤⡤⠟⠓⠒⠲⠤⠤⢿⣄⠀⠀⠀⠀⠀⠀⣏⣩⡤⠴⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⢠⣴⣿⣍⣻⠀⠀⠀⠀⣨⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠻⠿⢵⡿⠽⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⡤⢤⡶⢿⡽⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⡀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣙⣳⣿⣛⣙⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+
+def drawCharmeleon():
+  print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣧⢛⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⡖⠚⠁⠀⢺⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠛⠁⠀⠀⠀⠀⠀⠩⡥⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠇⣯⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⢯⡱⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣝⣮⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠶⠽⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣾⣼⣿⣼⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡷⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡷⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⡿⣿⢿⡿⣦⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣳⡽⢧⣟⣳⢮⣵⣢⢄⣢⢀⣀⠀⠀⠀⠀⠘⠿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢦⣿⡽⣯⣟⡼⣭⡟⡾⣭⢟⡾⣭⣗⡺⢤⣀⢀⠀⠀⢹⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠊⠵⡂⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢄⣾⣷⣿⣳⢾⣹⢧⣻⠽⣞⡽⣞⢧⣻⠽⣷⢮⡕⡅⡀⠈⢙⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢦⡿⣯⠷⣧⣻⢮⡗⣯⣳⢿⣹⡞⣭⡟⣞⣯⡳⢯⣷⡹⡐⠀⣼⠌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣢⣿⠿⣝⣯⣳⠽⣞⣽⣣⣟⢮⡷⣹⠷⣝⣧⣾⡽⣛⣶⡳⣧⠸⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡯⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⡿⣯⢟⣽⡲⣏⣿⣹⡶⠛⠚⢙⣿⣻⣿⡿⣯⣳⢽⣫⢶⡻⡽⡇⠻⣂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⢳⣛⣮⢷⣛⡶⢯⣧⠀⣀⣸⣌⣿⣿⢿⣳⣭⢷⣫⢷⡻⣽⠁⠀⢝⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢫⡄⠀⠀⠀⠀⢀⣀⣠⣀⣀⡀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣯⢯⡽⢮⣗⣻⡼⣿⡻⢿⣹⣏⡟⣿⢻⡾⣷⢞⣧⢟⡾⣵⡫⣀⣼⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡮⠃⠀⠀⣐⣾⡿⣟⣿⣻⡏⠁⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣯⣽⢻⡼⢧⣟⣵⢻⣏⢷⡞⣽⢞⣳⡽⣞⡯⡜⣭⠳⣍⢳⡙⣅⠀⠀⠀⠀⣤⢄⡀⠀⢀⣀⣀⠀⠀⠀⠀⠀⣴⠟⢁⣴⣧⣲⣾⣯⢿⣽⣾⣿⢷⣆⢄⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡾⣭⣻⣝⡾⣯⡷⣞⣯⢾⣝⣾⣣⣟⣾⡗⡹⢆⡻⣬⡧⡝⡟⠀⠀⠀⠀⢻⣞⣾⠺⣅⣀⣈⣷⠀⠀⢀⡀⠻⣲⣿⣻⣟⠈⣿⣽⣻⣞⡷⣯⢿⡽⣷⣦⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣹⣳⣳⢞⣽⢳⡟⣿⣻⢯⣭⣐⣱⣶⠟⠋⢿⣾⣥⣓⣫⠜⠀⠀⠀⠀⣠⠔⠛⢛⡟⣲⢶⣼⡿⢴⣷⣿⠃⠀⣙⣯⡷⣿⣞⣿⡳⠟⠾⡿⣽⢯⣟⣷⣻⣧
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣳⡽⢾⡭⣷⢻⣵⣫⢯⣽⣫⢷⣧⣄⣤⣠⣿⡇⠀⠀⠀⠀⠀⠀⢰⣻⣠⣾⣫⣾⣥⠻⠉⢣⠘⢋⠎⠀⠸⣿⠿⣽⢷⣻⣞⡿⠃⠀⠈⠟⠘⣿⢾⡽⣿
+  ⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣞⣯⣞⣳⢟⣞⢿⣻⢷⣿⣟⣾⡽⣯⣿⠟⠀⠀⠀⠀⠀⠀⠀⠈⢳⡹⡜⣽⣿⢻⡿⣿⣿⣿⠏⠀⠀⢀⣿⠈⠛⣿⡓⢹⠀⠀⠀⠀⠀⢠⣿⣯⠿⠀
+  ⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣟⡾⣽⢾⡽⣯⣿⣿⣿⣿⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣳⢳⡹⣻⣞⢯⣳⣟⣞⡁⣀⣀⣠⡾⠂⢀⣼⣿⠀⠈⠂⠀⠀⠀⠀⣿⣟⡁⠀⠀
+  ⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⢀⣴⣤⣗⡣⢧⠳⣽⣞⢯⣷⠫⠊⠁⠉⠁⠉⠀⣰⣿⢿⡽⣦⠀⠀⠀⠀⠀⠀⢻⣽⣗⠄⠀
+  ⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣆⡀⠀⣀⣼⣿⣿⣿⣿⣿⣿⣿⣳⣾⡟⠁⠀⠀⠀⠀⠀⠀⢘⣿⡽⣯⢿⠋⠀⠀⠀⠀⠀⠀⢠⣿⣞⣿⡆
+  ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣫⢿⡗⣿⡹⣷⢺⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠸⣿⡽⣯⣿⠀⠀⠀⠀⠀⢀⣴⣿⣳⣯⢿⡇
+  ⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡿⢾⣭⣷⢫⢷⣿⢏⣟⣙⣲⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠘⠻⣽⢷⣯⢿⡷⣶⢿⣻⢿⣻⢷⡻⡾⠋⠀
+  ⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣬⡛⣭⣷⣞⠽⣾⠼⣽⠓⢻⢽⡇⢀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠫⠯⠾⠭⠬⠇⠀⠀⠀⠀⠀⠊⠟⠾⠯⢻⢽⣯⣟⣻⠙⠟⠁⠀⠀⠀
+  ⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣶⣿⠈⣁⠸⠈⠘⣂⠸⠶⡼⣀⣼⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⠟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⢾⣽⠀⠀⠀⠀⠀⠀
+  ⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⠾⢿⠧⣌⣡⠇⡞⡁⣥⡆⣮⠉⣫⠉⠉⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢌⡿⢧⣿⠀⠀⠀⠀⠀⠀
+  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣎⣘⡔⣳⢉⢂⢉⣥⣴⣷⣧⣾⣿⣧⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⣽⣛⣾⠀⠀⠀⠀⠀⠀
+  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⣽⣿⣿⡿⠀⠀⠀⠀⠀⠀
+  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⡼⣟⡽⣫⣟⢿⣹⡝⣿⣜⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡽⣻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⣏⣿⠿⣜⣯⠃⠀⠀⠀⠀⠀⠀
+  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣭⢷⣻⡼⣝⡷⣺⢏⡷⣛⣮⡽⣎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡾⣝⣻⣦⣀⡀⠀⠀⠀⠀⠀⡀⠀⠀⣀⡤⡔⣏⢷⣛⡾⣝⣯⠎⠀⠀⠀⠀⠀⠀⠀
+  ⢘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢿⡞⣵⣛⣮⠷⣏⣯⡽⣛⡶⣏⡷⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣗⢯⣳⠿⣟⡿⣟⡿⣳⢖⡶⣩⣏⣝⣦⣱⣵⡟⣯⣳⢽⣞⠏⠀⠀⠀⠀⠀⠀⠀⠀
+  ⢸⣿⣟⣿⢻⡟⣿⡻⣿⣯⢽⣻⣯⡽⣯⡿⢧⠿⣜⡿⣹⢶⡻⣭⢷⣫⡽⣇⣿⣿⠿⠛⠋⠉⢰⡿⡽⣞⠾⣭⢷⣫⢿⣽⣻⣽⣳⢯⡽⣳⡽⣚⡷⣫⢶⡻⣵⣫⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠘⣿⣞⣯⡽⣶⠿⣽⣯⣗⠛⢿⢿⢻⣽⣿⡻⣝⡾⣭⢷⡻⡵⣏⡷⣝⣯⣼⡀⠀⠀⠀⣰⣿⡻⣵⢯⣻⡝⣞⣷⢺⠯⣷⣯⣟⣯⣞⣷⣹⢏⡷⢯⣳⡿⡷⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠙⢾⣽⣻⠙⣆⠸⢯⠐⢦⡀⢇⠁⢿⣷⡻⣝⡾⡵⢯⣳⢟⣽⡺⣝⣶⢫⣇⢀⣤⣾⣟⡷⣏⣷⢫⣷⣹⣏⣾⣓⣿⣦⣌⠉⠛⠚⠓⠛⢛⣋⠭⠓⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠉⠳⢄⠑⠸⠑⠤⣉⢻⠉⠚⣻⣿⣱⢯⣻⡝⣧⡟⣶⢻⣝⣮⣟⣝⣿⢿⢾⣽⣞⣿⡼⣛⣶⡽⣼⡟⠉⠌⡙⢏⠛⢽⠹⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠑⠒⠁⠀⠈⠉⠀⠀⠋⢯⣟⡿⣶⢿⣱⠿⣭⠿⢶⣿⢻⣄⠈⢿⡏⠺⢾⢷⢿⣿⡙⠳⢬⣿⡖⠒⠊⠭⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠻⣽⣿⠙⢦⢀⠱⠦⠬⢍⣒⣫⠉⠀⠁⠁⠀⠉⠉⠉⠉⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠢⠬⢘⣣⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+
+#Init
+import random
+import time
+import math
+pokemonLevel = 0 #Global
+pokemonXP = 0
+pokemonEvo = 0
+pokemonName = "Charmander"
+isQuit = 0
+day = 1
+wins = 0
+losses = 0
+winstreak = 0
+
+#Functions
+
+#Main
+print("Welcome to Pokemon Evolution!")
+
+while isQuit == 0:
+  #Introduction and ensures xprequirement is scaling with pokemon level.
+
+  xpRequirement = 100 + (3 * pokemonLevel)
+  print("It is day " + str(day))
+  print("Your Pokemon is Level " + str(pokemonLevel) + " with " + str(pokemonXP) + " XP")
+  print("You need " + str(xpRequirement - pokemonXP) + " XP to level up to Level " + str(pokemonLevel + 1))
+  print("Select an activity for the day")
+
+  print("""
+  1. Train
+  2. Gym Battle
+  3. Rest (Display Info)
+  4. Quit
+  """)
+
+  selectedActivity = int(input("Select a number 1-4"))
+  if selectedActivity == 1:
+    #If train is selected, pokemon will do between 4-9 pushups and gain 40-90 xp.
+
+    pokemonPushupsDone = random.randint(4,9)
+    pokemonXP = pokemonXP + (pokemonPushupsDone * 10)
+    time.sleep(0.5)
+    print("Congrats! Your Pokemon did " + str(pokemonPushupsDone) + " Pushups and gained " + str(pokemonPushupsDone * 10) + " XP!")
+    day = day + 1
+    if pokemonXP >= xpRequirement:
+      pokemonXP = pokemonXP - xpRequirement
+      pokemonLevel = pokemonLevel + 1
+      print("Congrats! Your Pokemon leveled up to level " + str(pokemonLevel))
+    time.sleep(1)
+    xpRequirement = 100 + (3 * pokemonLevel)
+    print("")
+
+  if selectedActivity == 2:
+    #If battle is selected, pokemon will enter a battle with another pokemon of (somewhat) random level.
+
+    opponentLevel = math.trunc(round(0.9 * pokemonLevel + random.randint(-3,3) , 0))
+
+    if opponentLevel < 1:
+      opponentLevel = 1
+
+    print("A Level " + str(opponentLevel) + " Pokemon has appeared! Do you want to fight?")
+    isBattle = int(input("Select 1 to battle. Any other key will retreat your Pokemon"))
+
+    #Three attacks have different levels of risk
+
+    if isBattle == 1:
+      print("You are in battle! Select an attack!")
+      print("""
+      1. Whip (Least risky, least potential XP)
+      2. Slash (Medium risk, medium potential XP)
+      3. Roar (Most risky, most potential XP)
+      """)
+      attack = int(input("Select 1, 2, or 3"))
+
+      successChance = math.trunc(round((90 - (attack * 20)) + (5 * (pokemonLevel - opponentLevel)) + (pokemonLevel ** 0.5) + (5 * pokemonEvo) , 0))
+      if successChance > 90:
+        successChance = 90
+
+      #Displays what player and opponent got. Higher number wins
+
+      successNumber = random.randint(1,100)
+      time.sleep(0.5)
+      print("You got a " + str(successChance))
+      time.sleep(0.5)
+      print("Your opponent got a " + str(successNumber))
+      time.sleep(1)
+      print("")
+
+      if successChance >= successNumber:
+        #What happens when your pokemon wins
+
+        print("CONGRATS! You won the fight!")
+        pokemonXP = pokemonXP + ((attack * 90) + (20 * (opponentLevel - pokemonLevel))) + (random.randint(0,25) * attack + (winstreak * 5))
+
+        while pokemonXP >= xpRequirement:
+          pokemonXP = pokemonXP - xpRequirement
+          pokemonLevel = pokemonLevel + 1
+          xpRequirement = 100 + (3 * pokemonLevel)
+          print("Your Pokemon leveled up!")
+          time.sleep(0.25)
+
+        print("")
+        print("Your Pokemon leveled up to Level " + str(pokemonLevel) + "!")
+
+        wins = wins + 1
+        winstreak = winstreak + 1
+
+      else:
+        #What happens when cpu pokemon wins
+
+        print("You lost the fight and gained no XP :(")
+        losses = losses + 1
+        winstreak = 0
+
+      day = day + 1
+      print("")
+      time.sleep(1)
+
+
+    else:
+      print("You have retreated!")
+      day = day + 1
+      time.sleep(1)
+
+  if selectedActivity == 3:
+    #Displays pokemon stats and draws selected pokemon
+
+    print("POKEMON STATS FOR " + pokemonName)
+    print("")
+    time.sleep(0.5)
+    print("Your Pokemon is Level " + str(pokemonLevel))
+    time.sleep(0.5)
+    print("Your Pokemon has " + str(pokemonXP) + " XP")
+    time.sleep(0.5)
+    print("Your Pokemon needs " + str(xpRequirement - pokemonXP) + " XP to level up to Level " + str(pokemonLevel))
+    time.sleep(0.5)
+    print("Your Pokemon has a " + str(wins) + "-" + str(losses) + " record. Your Pokemon is on a " + str(winstreak) + " game winstreak")
+    time.sleep(1.5)
+
+    if pokemonEvo == 0:
+      drawCharmander()
+    if pokemonEvo == 1:
+      drawCharmeleon()
+    if pokemonEvo == 2:
+      drawCharizard()
+
+    time.sleep(2)
+
+    input("Hit ENTER to continue")
+
+  if selectedActivity == 5:
+    print("This has not been coded yet!")
+
+  if selectedActivity == 4:
+    isQuit = 1
+
+  if selectedActivity == 6:
+    pokemonLevel = pokemonLevel + 25
+
+
+  #Evolves into charmeleon if pokemon level is high enough
+  if pokemonEvo == 0 and pokemonLevel >= 30:
+    pokemonLevel = pokemonLevel - 30
+    pokemonXP = 0
+    pokemonEvo = 1
+    pokemonName = "Charmeleon"
+    time.sleep(1)
+    print("""
+    Congrats! You leveled up to Charmeleon!
+    Your Pokemon's levels have been reset. Reach level 55 to evolve to Charizard!
+    """)
+    time.sleep(3)
+
+  #Evolves into charizard if charmeleon pokemon level is high enough
+  if pokemonEvo == 1 and pokemonLevel >= 55:
+    pokemonLevel = pokemonLevel - 55
+    pokemonXP = 0
+    pokemonEvo = 2
+    pokemonName = "Charizard"
+    time.sleep(1)
+    print("""
+    Congrats! You leveled up to Charizard!
+    Your Pokemon's levels have been reset. You have reached the max evolution!
+    """)
+    time.sleep(2)
+
+
